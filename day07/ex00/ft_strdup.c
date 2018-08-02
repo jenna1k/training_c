cddbea547eb60f5ac835c385378216e95ead9403
JennaKim <jenna_kim1@hotmail.com>
2018-08-02 14:51:41 -0700

    edited for norm

--- day07/ex00/ft_strdup.c
+++ day07/ex00/ft_strdup.c
@@ -6,33 +6,32 @@
 /*   By: dakim <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/02 11:56:58 by dakim             #+#    #+#             */
-/*   Updated: 2018/08/02 11:57:00 by dakim            ###   ########.fr       */
+/*   Updated: 2018/08/02 14:48:42 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <string.h>
 
-char *ft_strcpy(char *dest, char *src)
+char	*ft_strcpy(char *dest, char *src)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
-char *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-    char *str;
-    
-    str = (char *)malloc(sizeof(*str));
-    ft_strcpy(str, src);
+	char *str;
 
-    return(str);
+	str = (char *)malloc(sizeof(*str));
+	ft_strcpy(str, src);
+	return (str);
 }
