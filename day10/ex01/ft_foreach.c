d317101ee3e2ee9cd47ff6c6cb860d04d2c88616
JennaKim <jenna_kim1@hotmail.com>
2018-08-06 19:25:46 -0700

    Update ft_foreach.c

--- day10/ex01/ft_foreach.c
+++ day10/ex01/ft_foreach.c
@@ -9,15 +9,28 @@
 /*   Updated: 2018/08/06 17:51:13 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+// #include <stdio.h>
 
-void	ft_foreach(int *tab, int length, void (*f)(int))
+// void     ft_putnbr(int n)
+// {
+//     printf("%d,", n);
+// }
+
+void        ft_foreach(int *tab, int length, void(*f)(int))
 {
-	int i;
+    int i;
 
-	i = 0;
-	while (i < length)
-	{
-		f(tab[i]);
-		i++;
-	}
+    i = 0;
+    while (i < length)
+    {
+        f(tab[i]);
+        i++;
+    }
 }
+
+// int     main(void)
+// {
+//     int tab[] = {1, 2, 3, 4, 5};
+//     ft_foreach(tab, 5, &ft_putnbr);
+
+// }
