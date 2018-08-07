18399d4f7a07f6d436a85a8d16bcf902d4bd09b6
JennaKim <jenna_kim1@hotmail.com>
2018-08-06 21:33:17 -0700

    Add files via upload

--- day10/ex02/ft_map.c
+++ day10/ex02/ft_map.c
@@ -9,13 +9,43 @@
 /*   Updated: 2018/08/06 19:09:02 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int		addfive(int n)
+{
+	return(n + 5);
+}
 
 int     *ft_map(int *tab, int length, int(*f)(int))
 {
-    int *array;
+	int i;
+	int *array;
 
-    array = (*int)malloc(sizeof(*tab) + 1); // or length or without adding 1
-    *array = f(array);
+	i = 0;
+	array = malloc(sizeof(int) * length);
+	while (i < length)
+	{
+		array[i] = f(tab[i]);
+		i++;
 
-    return (array);
+	}
+
+	return (array);
+}
+
+int     main(void)
+{
+	int i;
+	int array[] = {5, 10, 15};
+
+	i = 0;
+	int *arr = ft_map(array, 3, &addfive);
+	while (i < 3)
+	{
+		printf("%d", arr[i]);
+		i++;
+	}
+	return (0);
 }
